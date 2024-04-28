2ba5a5cd114630980c61f4176f0358b4b62ffbda
Ehab Kamel <Hi1234hi9009@gmail.com>
2024-04-27 21:30:48 -0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <div>
       <h1>Hi</h1>
-      <script src="http://localhost:3000//089d1bfdb4e54328a59574fb7ac4e473b32134ed9982a399929388d97e08f2dd/inject.js"></script>
+      <script src="http://localhost:3000/089d1bfdb4e54328a59574fb7ac4e473b32134ed9982a399929388d97e08f2dd/inject.js"></script>
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    default-src 'none';
+    script-src 'none';
     style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
+    img-src 'self';
     font-src 'self';
     object-src 'none';
     base-uri 'self';
