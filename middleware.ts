d7b8fd1d2d319f5365064d764bfb42c79c43a0d0
Ehab Kamel <Hi1234hi9009@gmail.com>
2024-04-27 21:24:35 -0400

    hiu

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,6 @@ export function middleware(request: NextRequest) {
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
-    '/089d1bfdb4e54328a59574fb7ac4e473b32134ed9982a399929388d97e08f2dd/inject.js';
 `;
   // Replace newline characters and spaces
   const contentSecurityPolicyHeaderValue = cspHeader
